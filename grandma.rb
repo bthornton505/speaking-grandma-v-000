0c49aec27c527ce487ef30e11d529e12fe73d0a0
Brenden Thornton <thorntonbrenden@gmail.com>
2018-07-11 03:47:01 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,13 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
   if phrase.downcase
-    puts "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
     
   elsif phrase.upcase 
-    puts "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
     
   elsif phrase
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   end 
 end 
 
